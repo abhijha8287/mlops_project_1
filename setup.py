5503155f37c473fdfe74544938282dacf9059063
Abhishek Jha <abhijha8287@gmail.com>
2025-12-27 11:34:57 +0530

    upto experiment done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhishek Jha",
+    author_email="abhijha8287@gmail.com",
+    packages=find_packages()
+)
